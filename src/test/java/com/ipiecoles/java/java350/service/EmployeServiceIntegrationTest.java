02fb8a090fefd90e32bbb940fd525eac1874ce77
qmoriceau <quentin.moriceau@ipilyon.net>
2021-02-22 19:16:51 +0100

    quest 4

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
@@ -1,21 +1,25 @@
-/* package com.ipiecoles.java.java350.service;
+package com.ipiecoles.java.java350.service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import com.ipiecoles.java.java350.exception.EmployeException;
 import com.ipiecoles.java.java350.model.Employe;
-import com.ipiecoles.java.java350.model.Entreprise;
 import com.ipiecoles.java.java350.model.NiveauEtude;
 import com.ipiecoles.java.java350.model.Poste;
 import com.ipiecoles.java.java350.repository.EmployeRepository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class EmployeServiceIntegrationTest {
     @Autowired
     EmployeService employeService;
@@ -23,17 +27,28 @@ public class EmployeServiceIntegrationTest {
     @Autowired
     private EmployeRepository employeRepository;
 
+    @BeforeEach
+    @AfterEach
+    public void purge(){
+        employeRepository.deleteAll();
+    }
+
     @Test
     public void testIntegrationCalculPerformanceCommercial() throws EmployeException {
         // Given
-        employeRepository.save(new Employe("Moriceau", "Quentin", "C00001", LocalDate.now(), Entreprise.SALAIRE_BASE, Entreprise.PERFORMANCE_BASE, 1.0));
-        employeService.embaucheEmploye("Moriceau", "Quentin", Poste.COMMERCIAL, NiveauEtude.BAC, 1.0);
-        //When
-        Employe employe = employeRepository.findByMatricule("C00001");
-        employeService.calculPerformanceCommercial("C00001", 1000L, 1000L);
+        String nom = "Doe";
+        String prenom = "John";
+        Poste poste = Poste.COMMERCIAL;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 1.0;
 
+        //When
+        employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+        List<Employe> employes = employeRepository.findAll();
+        Assertions.assertThat(employes).hasSize(1);
+        Employe employe = employes.get(0);
+        employeService.calculPerformanceCommercial(employe.getMatricule(), 1000L, 1000L);
         //Then
-        
-        Assertions.assertThat(employe.getPerformance()).isEqualTo(2);
+        Assertions.assertThat(employe.getPerformance()).isEqualTo(1);
     }
-} */
+}
